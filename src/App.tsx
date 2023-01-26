7d4ea834e731ecbb83172b74b54d51bb14ab0c9c
rafael.vaca <revaca@pucese.edu.ec>
2023-01-26 11:21:31 -0500

    Refresh..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@nextui-org/react'
+import { Button, Input } from '@nextui-org/react'
 import React, { useState } from 'react'
 import RepoCard from './components/RepoCard'
 import { useFetchRepositories } from './hooks/useRepos'
@@ -10,14 +10,21 @@ function App() {
   if (isLoading) return <div>Loading...</div>
 
   return (
-    <div style={{ display: 'grid', justifyItems: 'center' }}>
-      <Input clearable bordered labelPlaceholder="Github username" initialValue="" onChange={({ target }) => {
-        setUsername(target.value)
-        mutate()
-      }} />
-      {data?.map((repo) => (
-        <RepoCard key={repo.id} repository={repo} />
-      ))}
+    <div style={{ display: 'grid', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <Input style={{ color: 'white' }} color='primary' clearable bordered labelPlaceholder="Github username" initialValue="" onChange={({ target }) => {
+          setUsername(target.value)
+        }} />
+        <Button disabled={username === ''} onClick={() => mutate()} animated>Search Repositories</Button>
+      </div>
+      {
+        data && data?.length > 0 &&
+        <div style={{ padding: '2rem' }}>
+          {data?.map((repo) => (
+            <RepoCard key={repo.id} repository={repo} />
+          ))}
+        </div>
+      }
     </div>
   )
 }
