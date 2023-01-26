7a86c3e8cf3d747f71b2baf355c000373658fe1a
rafael.vaca <revaca@pucese.edu.ec>
2023-01-26 13:50:43 -0500

    Console loco

--- src/main.tsx
+++ src/main.tsx
@@ -16,8 +16,6 @@ const darkTheme = createTheme(
     }
   })
 
-console.log(darkTheme)
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
